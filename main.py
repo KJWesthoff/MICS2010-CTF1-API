ddb61a986ae862510e7f16c8dbe17850b7f3d3f9
KJWesthoff <karl.johan.westhoff@gmail.com>
2024-05-16 12:39:23 -0700

    changed route for query parameters

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ app = FastAPI()
 
 @app.get('/')
 def read_root():
-    return {"Hello": "Fast Api World"}
+    return {"Hello": "Fast Api World Test"}
 
 
 
-@app.get('/{op}/{x}/{y}', response_class=PlainTextResponse )
+@app.get('/math', response_class=PlainTextResponse )
 def read_simplemaths(op,x:int,y:int):
     if op == "plus":
         res = x+y
